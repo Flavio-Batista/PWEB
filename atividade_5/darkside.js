b6afde74d7fc029e1d41fdfeb3c68dc8a551468f
Flavio Batista <flavio.rod.batista@gmail.com>
2021-03-17 22:04:18 -0300

    ok

---
+++ atividade_5/darkside.js
@@ -0,0 +1,16 @@
+alert("Junte-se ao lado negro da força!");
+
+var nome;
+var floatMedia1;
+var floatMedia2;
+var floatMedia3;
+
+nome = prompt("Digite seu nome jovem jedi!!!");
+alert(`Boa noite jovem jedi: ` + nome );
+floatMedia1 = prompt("Sua primeira nota em Lambda-class shuttle?");
+floatMedia2 = prompt("Sua segunda nota em It's a Trap!");
+floatMedia3 = prompt("Sua terceira nota em i'm Highground now?");
+
+media = parseFloat(((floatMedia1 + floatMedia2 + floatMedia3)/3));
+alert("Sua nota jedi!!! " + media);
+
