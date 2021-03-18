c9bbf4fe9e8803d12fb5392313c1569ab44977d0
Flavio Batista <flavio.rod.batista@gmail.com>
2021-03-17 22:21:06 -0300

    partiu!!

---
+++ atividade_5/commando.js
@@ -0,0 +1,24 @@
+alert("Comando chamando Rambo!!! Rambo Responda!!!");
+
+var floatNum1;
+var floatNum2;
+var soma;
+var sub;
+var prod;
+var div;
+var resto;
+
+floatNum1 = parseFloat(prompt("Numero da sua primeira missão?"));
+floatNum2 = parseFloat(prompt("Numero da sua segunda missão?"));
+
+soma = floatNum1 + floatNum2;
+sub = floatNum1 - floatNum2;
+prod = floatNum1 * floatNum2;
+div = floatNum1/floatNum2;
+resto = floatNum1%floatNum2;
+
+alert("Sua nota Rambo!!! " + soma);
+alert("Sua nota Rambo!!! " + sub);
+alert("Sua nota Rambo!!! " + prod);
+alert("Sua nota Rambo!!! " + div);
+alert("Sua nota Rambo!!! " + resto);
