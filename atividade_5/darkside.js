b6cce481efd77f431b01f0b0590e27ebc89b9c00
Flavio Batista <flavio.rod.batista@gmail.com>
2021-03-17 22:08:30 -0300

    agora foi

--- atividade_5/darkside.js
+++ atividade_5/darkside.js
@@ -7,9 +7,9 @@ var floatMedia3;
 
 nome = prompt("Digite seu nome jovem jedi!!!");
 alert(`Boa noite jovem jedi: ` + nome );
-floatMedia1 = prompt("Sua primeira nota em Lambda-class shuttle?");
-floatMedia2 = prompt("Sua segunda nota em It's a Trap!");
-floatMedia3 = prompt("Sua terceira nota em i'm Highground now?");
+floatMedia1 = parseFloat(prompt("Sua primeira nota em Lambda-class shuttle?"));
+floatMedia2 = parseFloat(prompt("Sua segunda nota em It's a Trap!"));
+floatMedia3 = parseFloat(prompt("Sua terceira nota em i'm Highground now?"));
 
 media = parseFloat(((floatMedia1 + floatMedia2 + floatMedia3)/3));
 alert("Sua nota jedi!!! " + media);
